623503749c9b035f4892bca13f846f90b99da059
sindhuchoubey546 <sindhuchoubey@gmail.com>
2018-07-07 11:43:20 +0545

    first commit

---
+++ caesar.js
@@ -0,0 +1,31 @@
+function encrypt(msg, key)
+{
+    var encMsg = "";
+
+    for(var i = 0; i < msg.length; i++)
+    {
+        var code = msg.charCodeAt(i);
+
+        // Encrypt only letters in 'A' ... 'Z' interval
+        if (code >= 65 && code <= 65 + 26 - 1)
+        {
+            code -= 65;
+            code = mod(code + key, 26);
+            code += 65;
+        }
+
+        encMsg += String.fromCharCode(code);
+    }
+
+    return encMsg;
+}
+
+
+// Modulo function: n mod p
+function mod(n, p)
+{
+    if ( n < 0 )
+        n = p - Math.abs(n) % p;
+
+    return n % p;
+}
